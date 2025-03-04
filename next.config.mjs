62b29b153fed12099158c3926490aadb3b5f4f6d
Santha-Ashwin-I-A <santhawebdev@gmail.com>
2025-03-04 22:10:31 +0530

    mid-stage

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint:{
+        ignoreDuringBuilds:true,
+    },
     modularizeImports:{
         lodash:{
             transform:'lodash/{{member}}'
