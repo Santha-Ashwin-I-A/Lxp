7e5bf0696e7692bb3bc2a75542878279c5212583
Santha-Ashwin-I-A <santhawebdev@gmail.com>
2025-03-05 22:04:25 +0530

    mid-stage

--- app/(dashboard)/(routes)/search/_components/categories.tsx
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -4,6 +4,7 @@ import { Category } from "@prisma/client";
 import {FcEngineering,FcFilmReel,FcMultipleDevices,FcMusic,FcOldTimeCamera,FcSalesPerformance,FcSportsMode}from "react-icons/fc"
 import { IconType } from "react-icons";
 import { CategoryItem } from "./category-item";
+import { Suspense } from "react";
 
 interface CategoriesProps {
     items: Category[];
@@ -23,15 +24,17 @@ export const Categories = ({
     items,
 }:CategoriesProps) =>{
     return (
-        <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
-            {items.map((item)=>(
-                <CategoryItem
-                    key={item.id}
-                    label={item.name}
-                    icon ={iconMap[item.name]}
-                    value={item.id}
-                />
-            ))}
-        </div>
+        <Suspense fallback={<div>Loading</div>}>
+            <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+                {items.map((item)=>(
+                    <CategoryItem
+                        key={item.id}
+                        label={item.name}
+                        icon ={iconMap[item.name]}
+                        value={item.id}
+                    />
+                ))}
+            </div>
+        </Suspense>
     )
 }
