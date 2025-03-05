dc02b550374c1a41cecfb4c600fc312d3b907c60
Santha-Ashwin-I-A <santhawebdev@gmail.com>
2025-03-05 21:30:00 +0530

    mid-stage

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,57 +1,17 @@
-import { db } from "@/lib/db";
-import { Categories } from "./_components/categories";
-import { SearchInput } from "@/components/search-input";
-import { getCourses } from "@/actions/get-courses";
-import { auth } from "@clerk/nextjs/server";
-import { CouresList } from "@/components/courses-list";
-import { redirect } from "next/navigation";
 import {Suspense} from "react";
+import { SearchPage } from "./_components/search";
 
 type SearchPageProps= Promise<{
         title: string;
         categoryId: string;
 }>
 
-const SearchPage = async({
+const Spage = ({
     searchParams
-}:{searchParams:SearchPageProps}) => {
-    const user = auth();
-    const userId = (await user).userId;
-    const categories = await db.category.findMany({
-        orderBy:{
-            name:"asc"
-        }
-    });
-    if(userId == null){
-        console.log("No users found")
-        return redirect("/")
-    }
-    const params = await searchParams;
-    const courses = await getCourses({
-        userId,
-        ...params,
-    });
-
-    return ( 
-        <>
-            <div className="px-6 pt-6 md:hidden md:mb-0 block">
-                <SearchInput/>
-            </div>
-            <div className="p-6 space-y-4">
-                <Categories
-                    items={categories}
-                />
-                <CouresList
-                    items={courses}
-                />
-            </div>
-        </>
-     );
-}
-const Spage = ({props}:{props:SearchPageProps})=>{
+}:{searchParams:SearchPageProps})=>{
     return (
         <Suspense fallback={<div>Loading...</div>}>
-          <SearchPage searchParams={props} />
+          <SearchPage searchParams={searchParams} />
         </Suspense>
       )
 }
